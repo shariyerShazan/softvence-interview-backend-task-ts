69668aa68afe5587c307d4d0259110d6d71a3726
shariyerShazan <shariyershazan1@gmail.com>
2025-10-06 19:36:18 +0600

    product routes

---
+++ routes/product.routes.ts
@@ -0,0 +1,18 @@
+import express, { Router } from "express";
+import { isAuthed } from "../middlewares/isAuthed.js";
+import { isVendor } from "../middlewares/role.middleware.js";
+import {
+  addProduct,
+  deleteProduct,
+  getAllProducts,
+  updateProduct,
+} from "../controllers/product.controller.js";
+
+const route: Router = express.Router();
+
+route.post("/", isAuthed, isVendor, addProduct);
+route.get("/", getAllProducts);
+route.delete("/:productId", isAuthed, isVendor, deleteProduct);
+route.patch("/:productId/update", isAuthed, isVendor, updateProduct);
+
+export default route;
