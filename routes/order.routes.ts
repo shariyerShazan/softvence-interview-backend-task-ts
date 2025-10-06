75e7669bdbbfc73e2bdcfb3f325e30cb14bb92f3
shariyerShazan <shariyershazan1@gmail.com>
2025-10-06 19:35:47 +0600

    order routes

---
+++ routes/order.routes.ts
@@ -0,0 +1,16 @@
+import express, { Router } from "express";
+import { isAuthed } from "../middlewares/isAuthed.js";
+import { isUser, isVendor } from "../middlewares/role.middleware.js";
+import {
+  createOrder,
+  getOrders,
+  updateOrderStatus,
+} from "../controllers/order.controller.js";
+
+const route: Router = express.Router();
+
+route.post("/", isAuthed, isUser, createOrder);
+route.get("/", isAuthed, getOrders);
+route.post("/:orderId/status", isAuthed, isVendor, updateOrderStatus);
+
+export default route;
